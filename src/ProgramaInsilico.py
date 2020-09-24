0d96f68e579360dc9a7ba7b2535a732628ec4404
fabel134 <fabel134@gmail.com>
2020-09-24 08:50:42 -0500

    actulice el programa python

--- src/ProgramaInsilico.py
+++ src/ProgramaInsilico.py
@@ -7,16 +7,28 @@
 
 #print(My_genome)
 
-f = open ("c:/Users/jan/GIT/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta ", "r") 
+f = open ("c:/Users/Jose Abel/BetterLab/Git/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta ", "r") 
 #Recuerda que para los path de python en windows debes poner "C:"
-print(f.read())
+#print(f.read())
+lineas = f.readlines()
+#print(lineas)
+
+for i in lineas:
+    #print(i)
+    if i == ">" :
+        print(i)
+    else :
+        print(i)
+#for i in lineas:
+ #   if i = ">":
+        #print(m, 'is large')
+    #else:
+        #print(m, 'is small')
+
+#for i in lineas:
+#if i = @^>
+#print ("hola")
 
 #Cambiar el nombre del archivo
 #linea = f.readlines() #lee el archivo linea por linea
 #print(linea)
-
-
-
-
-
-
