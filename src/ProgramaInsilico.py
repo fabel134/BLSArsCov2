9f00cc3e29d96d00ed909372da1f66633ed0eec7
fabel134 <fabel134@gmail.com>
2020-09-24 16:06:20 -0500

    encontramos un nuevo problema

--- src/ProgramaInsilico.py
+++ src/ProgramaInsilico.py
@@ -6,19 +6,25 @@
 #with open ("c:/Users/jan/GIT/BLSarscov2/data/demofile.txt", "w") as My_genome
 
 #print(My_genome)
-
-f = open ("c:/Users/Jose Abel/BetterLab/Git/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta ", "r") 
-#Recuerda que para los path de python en windows debes poner "C:"
+import re
+Vieja = open ("c:/Users/jan/GIT/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta", "r")
+#Nueva = open ("c:/Users/Jose Abel/BetterLab/Git/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta ", "r") 
+    #Recuerda que para los path de python en windows debes poner "C:"
 #print(f.read())
-lineas = f.readlines()
-#print(lineas)
+lineas = Vieja.readlines()
+print(lineas)
 
-for i in lineas:
+for l in lineas:
+    x = re.search("ATG", l)
+    y = bool(x)
+print("x es", y)
+
+#for i in lineas:
     #print(i)
-    if i == ">" :
-        print(i)
-    else :
-        print(i)
+    #if i == ">" :
+        #print(i)
+    #else :
+        #print(i)
 #for i in lineas:
  #   if i = ">":
         #print(m, 'is large')
@@ -29,6 +35,3 @@ for i in lineas:
 #if i = @^>
 #print ("hola")
 
-#Cambiar el nombre del archivo
-#linea = f.readlines() #lee el archivo linea por linea
-#print(linea)
