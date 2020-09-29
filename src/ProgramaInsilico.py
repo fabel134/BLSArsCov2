589daf07343f5b744e81bbe6dc995d7507c8f699
fabel134 <fabel134@gmail.com>
2020-09-28 23:37:30 -0500

    Tenemos una problema con la expresiones regualares :O

--- src/ProgramaInsilico.py
+++ src/ProgramaInsilico.py
@@ -7,18 +7,19 @@
 
 #print(My_genome)
 import re
-Vieja = open ("c:/Users/jan/GIT/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta", "r")
-#Nueva = open ("c:/Users/Jose Abel/BetterLab/Git/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta ", "r") 
+#Vieja = open ("c:/Users/jan/GIT/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta", "r")
+Nueva = open ("c:/Users/Jose Abel/BetterLab/Git/BLSarscov2/data/gisaid_hcov-19_2020_08_07_19.fasta ", "r") 
     #Recuerda que para los path de python en windows debes poner "C:"
 #print(f.read())
-lineas = Vieja.readlines()
-print(lineas)
+#lineas = Vieja.readlines()
+lineas = Nueva.readlines()
+#print(lineas)
 
 for l in lineas:
-    x = re.search("ATG", l)
+    x = re.search("^>", l)
     y = bool(x)
 print("x es", y)
-
+print(x)
 #for i in lineas:
     #print(i)
     #if i == ">" :
