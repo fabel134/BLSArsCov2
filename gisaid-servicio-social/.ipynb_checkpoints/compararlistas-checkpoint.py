aea96bd8df2090306ce8b37d22237267079e43cf
fabel134 <fabel134@gmail.com>
2021-05-08 21:04:27 -0500

    Buscamos los IDs faltanres para subirlos a

---
+++ gisaid-servicio-social/.ipynb_checkpoints/compararlistas-checkpoint.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+archivo_nombre1 = '97-mal.txt'
+archivo = open(archivo_nombre1,mode='r')
+texto1 = archivo.read()
+archivo.close()
+# Podemos mostrar en pantalla el archivo que se acaba de cargar
+#print(texto1)
+
+archivo_nombre2 = '97.txt'
+archivo = open(archivo_nombre2,mode='r')
+texto2= archivo.read()
+archivo.close()
+# Podemos mostrar en pantalla el archivo que se acaba de cargar
+#print(texto2)
+
+
+lista1=set([texto1])
+lista2=set([texto2])
+print(lista1)
+final = lista1 & lista2
+if len(final) > 0:
+	print("hay {} elementos coincidentes".format(len(final)))
+	print(final)
+else:
+	print ("no hay repeticiones")
+
---
+++ src/BoletosRifa.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/env python3.7
+#-*- coding: iso-8859-1 -*-
+
+b= range(0,157)
+list(b)
+
